1264006654d708eb913a3e2ff863365b592ca32a
YCHAKER <simonneau.q@gmail.com>
2026-05-26 16:40:08 +0200

    v3.19: cercle TEST jaune PK100 + comptes features dans statut

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker – Carte LGV SEA */
-const CACHE = 'lgv-sea-v19';
+const CACHE = 'lgv-sea-v20';
 const TILE_CACHE = 'lgv-tiles-v1';
 
 /* Ne jamais pré-cacher le HTML : il doit toujours venir du réseau */
